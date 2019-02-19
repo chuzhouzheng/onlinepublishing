98b00682e78f5750ad56dd789ef180b69f605201
zhengchuzhou <chuzhou.zheng@hiii-life.com>
2019-02-20 00:35:47 +0800

    提交更新

---
+++ sql/envir.sql
@@ -0,0 +1,4 @@
+INSERT INTO `envir` VALUES (1, '开发环境');
+INSERT INTO `envir` VALUES (5, '测试环境');
+INSERT INTO `envir` VALUES (10, '预生产环境');
+INSERT INTO `envir` VALUES (15, '生产环境');
---
+++ sql/operate_type.sql
@@ -0,0 +1,4 @@
+INSERT INTO `operate_type` VALUES (1, '开发提交任务');
+INSERT INTO `operate_type` VALUES (5, '更新到测试环境');
+INSERT INTO `operate_type` VALUES (10, '更新到预生产环境');
+INSERT INTO `operate_type` VALUES (15, '更新到生产环境');
